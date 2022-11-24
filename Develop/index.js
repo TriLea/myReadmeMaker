f3bde1f4747771ee499bf9183f48f6c3c9b062cf
trista_lea <tristan_lea@outlook.com>
2022-11-23 21:24:10 -0800

    start of the project, created repository, added prompt boiler plate, setup package.json, and git ignore

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,24 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: 'checkbox',
+        name: 'sections',
+        message: 'Which sections do you want?',
+        choices: [
+        'table of contents','About', 'Installation', 'Usage', 'future',
+        ],
+    },
+];
+
+inquirer
+  .prompt(
+    questions,
+  )
+  .then(answers => {
+    console.info('Answer:', answers.sections);
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
