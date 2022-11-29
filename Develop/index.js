bfd6aa4414f4c457274a3e0b30114137a3d1ff73
trista_lea <tristan_lea@outlook.com>
2022-11-28 18:36:13 -0800

    got to the point at which I can start generating markdown to a global fileContent variable

--- Develop/index.js
+++ Develop/index.js
@@ -2,15 +2,27 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [{
-        type: 'checkbox',
-        name: 'sections',
-        message: 'Which sections do you want?',
-        choices: [
-        'table of contents','About', 'Installation', 'Usage', 'future',
-        ],
-    },
+const questions = [
+  {
+    type: 'input',
+    name: 'location',
+    message: 'Where would you like me to send your file?',
+  },
+  {
+    type: 'checkbox',
+    name: 'sections',
+    message: 'Which sections do you want?',
+    choices: [
+    'table of contents','About', 'Installation', 'Usage', 'future',
+    ],
+  },
 ];
+
+var questionTwo = 
+{
+  type: 'input',
+  name: 'content',
+};
 
 inquirer
   .prompt(
@@ -18,6 +30,28 @@ inquirer
   )
   .then(answers => {
     console.info('Answer:', answers.sections);
+    var fileText;
+
+    if (answers.sections[0] == 'table of contents')
+    {
+      for (var section in answers.sections)
+      {
+        if (section == 'table of contents')
+          continue;
+      }
+    }
+    
+    for (var section in answers.sections)
+    {
+      if (section == 'table of contents')
+        continue;
+      
+      questionTwo.message = 'Please type out your ' + section + ' section.';
+      inquirer.prompt(questionTwo).then(answers => 
+      {
+        
+      });
+    }
   });
 
 // TODO: Create a function to write README file
