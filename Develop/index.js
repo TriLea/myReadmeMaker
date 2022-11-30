ed2290083bc49b46634b01b1b531db6fdc1a10c6
trista_lea <tristan_lea@outlook.com>
2022-11-29 23:34:43 -0800

    fixed formating of titles for readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,15 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 var fs = require('fs');
+const { generateKey } = require('crypto');
 
 // TODO: Create an array of questions for user input
 const questions = [
-  {
-    type: 'input',
-    name: 'location',
-    message: 'Where would you like me to send your file?',
-  },
+  // {
+  //   type: 'input',
+  //   name: 'location',
+  //   message: 'Where would you like me to send your file?',
+  // },
   {
     type: 'checkbox',
     name: 'sections',
@@ -25,46 +26,55 @@ var questionTwo =
   name: 'content',
 };
 
+
+
 inquirer
   .prompt(
     questions,
   )
   .then(answers => {
-    console.info('Answer:', answers.sections);
-    var fileText = '';
+    console.log('Answer:', answers);
 
-    if (answers.sections[0] == 'Table Of Contents')
-    {
-      for (var section in answers.sections)
-      {
-        if (section == 'Table Of Contents')
-          fileText = '## Table Of Contents/n/n';
+    // var fileText = '';
+
+    // if (answers.sections[0] == 'Table Of Contents')
+    // {
+    //   for (var section in answers.sections)
+    //   {
+    //     if (section == 'Table Of Contents')
+    //       fileText = '## Table Of Contents/n/n';
         
-        else
-          fileText += '- [' + section + '](#' + section + ')/n';
-      }
-    }
+    //     else
+    //       fileText += '- [' + section + '](#' + section + ')/n';
+    //   }
+    // }
     
-    for (var section in answers.sections)
-    {
-      if (section == 'Table Of Contents')
-        continue;
+    // for (var section in answers.sections)
+    // {
+    //   if (section == 'Table Of Contents')
+    //     continue;
       
-      questionTwo.message = 'Please type out your ' + section + ' section.';
-      inquirer.prompt(questionTwo).then(answers => 
-      {
-        fileText += '/n## ' + section + '/n/n';
-        fileText += answers.content;
-      });
-    }
-    writeToFile(answers.location + '/README.md', fileText);
+    //   //questionTwo.message = 'Please type out your ' + section + ' section.';
+    //   inquirer.prompt(questionTwo).then(answers => 
+    //   {
+    //     fileText += '/n## ' + section + '/n/n';
+    //     fileText += answers.content;
+    //   });
+    // }
+    writeToFile('./dist/README.md', answers);
   });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) 
 {
+  var templateLit = `
+    ${data.sections.map((section)=>{
+
+      return `## ${section}\n`;
+    }).join('')}`
+
   try {
-    fs.writeFileSync(fileName, data);
+    fs.writeFileSync(fileName, templateLit);
     console.info('file written successfully');
 
   } catch (err) {
