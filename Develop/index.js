e45b3b4afe03bf07594cbab80a22a477f38e1eb0
trista_lea <tristan_lea@outlook.com>
2022-12-01 22:37:35 -0800

    gutted everything and removed everything that was ovr complicating it. Still need to fix md parse file, and write file path.

--- Develop/index.js
+++ Develop/index.js
@@ -1,91 +1,63 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 var fs = require('fs');
-const { generateKey } = require('crypto');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-  // {
-  //   type: 'input',
-  //   name: 'location',
-  //   message: 'Where would you like me to send your file?',
-  // },
   {
-    type: 'checkbox',
-    name: 'sections',
-    message: 'Which sections do you want?',
-    choices: [
-    'Table Of Contents','About', 'Installation', 'Usage', 'Future',
-    ],
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'Header'
   },
-];
 
-var questionTwo = 
-{
-  type: 'input',
-  name: 'content',
-};
+  {
+      type: 'input',
+      message: 'Explain your project?',
+      name: 'About'
+  },
 
+  {
+      type: 'input',
+      message: 'Describe how to install your project?',
+      name: 'installation'
+  },
 
+  {
+      type: 'input',
+      message: 'Descrie your usage?',
+      name: 'usage'
+  },
 
-inquirer
-  .prompt(
-    questions,
-  )
-  .then(answers => {
-    console.log('Answer:', answers);
+  {
+    type: 'input',
+    message: 'What is the future of your project?',
+    name: 'future'
+  },
 
-    // var fileText = '';
-
-    // if (answers.sections[0] == 'Table Of Contents')
-    // {
-    //   for (var section in answers.sections)
-    //   {
-    //     if (section == 'Table Of Contents')
-    //       fileText = '## Table Of Contents/n/n';
-        
-    //     else
-    //       fileText += '- [' + section + '](#' + section + ')/n';
-    //   }
-    // }
-    
-    // for (var section in answers.sections)
-    // {
-    //   if (section == 'Table Of Contents')
-    //     continue;
-      
-    //   //questionTwo.message = 'Please type out your ' + section + ' section.';
-    //   inquirer.prompt(questionTwo).then(answers => 
-    //   {
-    //     fileText += '/n## ' + section + '/n/n';
-    //     fileText += answers.content;
-    //   });
-    // }
-    writeToFile('./dist/README.md', answers);
-  });
+  {
+      type: 'list',
+      message: 'Select a license?',
+      name: 'license',
+      choices: ['MIT', 'Apache 2.0', 'GPLv2', 'The UnLicense']
+  },
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) 
 {
-  var templateLit = `
-    ${data.sections.map((section)=>{
-
-      return `## ${section}\n`;
-    }).join('')}`
-
-  try {
-    fs.writeFileSync(fileName, templateLit);
-    console.info('file written successfully');
-
-  } catch (err) {
-    console.error(err);
-  }
+  //return ???????
 }
+// file written successfully
 
-// TODO: Create a function to initialize app
+// // TODO: Create a function to initialize app
 function init() 
-{
-  
+{ 
+  inquirer.prompt(questions).then((answers) => 
+  {
+    console.log('README.MD Generated');
+    writeToFile('README.md', generateMarkdown({answers}));
+  });
 }
 
 // Function call to initialize app
